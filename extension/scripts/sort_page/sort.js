f286c639c3d002a2f42ddebd17d7d2cdbf4cee05
Jerel Henderson <259@holbertonschool.com>
2020-06-08 02:01:05 +0000

    Attempt Browserify sort.js again

--- extension/scripts/sort_page/sort.js
+++ extension/scripts/sort_page/sort.js
@@ -1,6 +1,26 @@
 let sw = require('stopword');
 let extractor = require('@knod/unfluff');
 
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
+        let url = tabs[0].url;
+        grabHTML(url);
+        alert(url);
+    });
+});
+
+function grabHTML(url) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = function() {
+        pageSource = xhr.responseText;
+	alert(pageSource);
+        getWords(pageSource);
+    };
+    xhr.open("GET", url);
+    xhr.send();
+};
+
 function getWords(data) {
     data = extractor(data);
     alert(data);
@@ -9,7 +29,7 @@ function getWords(data) {
     let wordsMap = countWords(wordsArray);
     let sortedList = sortByFreq(wordsMap);
 
-    console.log("/" + sortedList[0]["word"] + "/" + sortedList[1]["word"] + "-" + sortedList[2]["word"]); 
+    alert("/" + sortedList[0]["word"] + "/" + sortedList[1]["word"] + "-" + sortedList[2]["word"]); 
 };
 
 function splitWords (givenText) {
